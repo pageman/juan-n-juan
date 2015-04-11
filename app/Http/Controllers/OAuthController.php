4b8ff2e845454a1e869b4d9f7cbeaf9081593577
awkwardusername <awkwardusername@gmail.com>
2015-04-11 20:38:03 +0800

    fix for oauth

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -20,28 +20,27 @@ class OAuthController extends Controller {
 
             $user = User::where('email', $user_social->getEmail())->first();
 
-            if (isset($user)) {
-                Auth::login($user);
-            } else {
+            if (!isset($user)) {
                 $user           = new User;
                 $user->email    = $user_social->getEmail();
                 $user->name     = $user_social->getName();
                 $user->password = bcrypt(str_random());
                 $user->save();
 
-                $user_profile = new UserProfile;
+                $user_profile             = new UserProfile;
                 $user_profile->first_name = $user_social->user['first_name'];
-                $user_profile->last_name = $user_social->user['last_name'];
-                $user_profile->gender = $user_social->user['gender'];
-                $user_profile->avatar = $user_social->avatar;
-                $user_profile->link = $user_social->user['link'];
+                $user_profile->last_name  = $user_social->user['last_name'];
+                $user_profile->gender     = $user_social->user['gender'];
+                $user_profile->avatar     = $user_social->avatar;
+                $user_profile->link       = $user_social->user['link'];
                 $user->userProfile()->save($user_profile);
             }
+
+            Auth::login($user);
 
             return redirect('/');
         }
 
         return redirect('auth/login');
     }
-
 }
